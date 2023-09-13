43aac2ec850d26cf88a7fff33065f2b800aab85a
jia00012 <jia00012@gmail.com>
2023-09-13 17:56:42 -0400

    implement multiply operation

---
+++ src/calculator.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Calculator = void 0;
+var Calculator = /** @class */ (function () {
+    function Calculator() {
+    }
+    Calculator.prototype.add = function (x, y) {
+        return x + y;
+    };
+    Calculator.prototype.sub = function (x, y) {
+        return x - y;
+    };
+    Calculator.prototype.mult = function (x, y) {
+        return x * y;
+    };
+    return Calculator;
+}());
+exports.Calculator = Calculator;
+;
